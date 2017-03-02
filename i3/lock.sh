2b47c8232627927bd8589af301c309738bc2a4e0
Savu Victor-Gabriel <vsavu@ayoda.net>
2017-03-02 16:37:36 +0100

    speed up lock

--- i3/lock.sh
+++ i3/lock.sh
@@ -4,7 +4,7 @@
 scrot /tmp/screen_locked.png
 
 # Pixellate it 10x
-mogrify -blur 0x10 /tmp/screen_locked.png
+mogrify -blur 5x10 /tmp/screen_locked.png
 
 # Lock screen displaying this image.
 i3lock -i /tmp/screen_locked.png
