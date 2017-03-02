9a7a0820f2ed275eb0353fd1bd139abb5cfc5281
Savu Victor-Gabriel <vsavu@ayoda.net>
2017-03-02 16:37:59 +0100

    link i3 config and .Xresources

--- link.sh
+++ link.sh
@@ -1,4 +1,11 @@
-#!/bin/bash
+#!/bin/bash -e
+
+rm $HOME/.vimrc || true
+rm $HOME/.screenrc || true
+rm $HOME/.Xresources || true
+rm -r $HOME/.config/i3 || true
 
 ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
 ln -s $HOME/dotfiles/.screenrc $HOME/.screenrc
+ln -s $HOME/dotfiles/.Xresources $HOME/.Xresources
+ln -s $HOME/dotfiles/i3 $HOME/.config/i3
