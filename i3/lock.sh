048c87e12e76571e56a1d3611bf09fa81fff0a0c
Savu Victor-Gabriel <vsavu@ayoda.net>
2017-04-04 09:13:02 +0200

    lock faster

--- i3/lock.sh
+++ i3/lock.sh
@@ -9,4 +9,4 @@ mogrify -scale 20% -scale 500% /tmp/screen_locked.png
 # Lock screen displaying this image.
 i3lock -i /tmp/screen_locked.png
 
-sleep 10; pgrep i3lock && xset dpms force off
+sleep 2; pgrep i3lock && xset dpms force off
