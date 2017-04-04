e5dc2f9fd0652e3658899c5ac59a670631e28306
Savu Victor-Gabriel <vsavu@ayoda.net>
2017-04-04 09:11:42 +0200

    speedup and turn monitor off

--- i3/lock.sh
+++ i3/lock.sh
@@ -4,7 +4,9 @@
 scrot /tmp/screen_locked.png
 
 # Pixellate it 10x
-mogrify -blur 5x10 /tmp/screen_locked.png
+mogrify -scale 20% -scale 500% /tmp/screen_locked.png
 
 # Lock screen displaying this image.
 i3lock -i /tmp/screen_locked.png
+
+sleep 10; pgrep i3lock && xset dpms force off
